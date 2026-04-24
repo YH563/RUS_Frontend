cd6e9ceed6802243854361b112ccdf9ef918c03e
YH563 <1066948247@qq.com>
2026-04-24 21:20:21 +0800

    添加了控制相机运动的代码文件

--- src/UI/Camera/CameraInputControl.cs
+++ src/UI/Camera/CameraInputControl.cs
@@ -8,25 +8,25 @@ namespace SRC.UI.Camera
     /// <summary>
     /// 轨道相机交互
     /// </summary>
-    public partial class CameraInputControl : InputRegion
-    {
-        [Export] public Control CoverageControl;  // 用于获取屏幕矩形
+    //public partial class CameraInputControl : InputRegion
+    //{
+    //    [Export] public Control CoverageControl;  // 用于获取屏幕矩形
 
-        // 相机运动基础参数设置
-        [Export] public float MouseSensitivity = 0.3f;
-        [Export] public float ZoomSpeed = 0.5f;
-        [Export] public float MinSpringLength = 2.0f;
-        [Export] public float MaxSpringLength = 20.0f;
-        [Export] public float MinPitchAngle = -30.0f;
-        [Export] public float MaxPitchAngle = 60.0f;
+    //    // 相机运动基础参数设置
+    //    [Export] public float MouseSensitivity = 0.3f;
+    //    [Export] public float ZoomSpeed = 0.5f;
+    //    [Export] public float MinSpringLength = 2.0f;
+    //    [Export] public float MaxSpringLength = 20.0f;
+    //    [Export] public float MinPitchAngle = -30.0f;
+    //    [Export] public float MaxPitchAngle = 60.0f;
 
-        private float _yaw = 0f;
-        private float _pitch = 0f;
+    //    private float _yaw = 0f;
+    //    private float _pitch = 0f;
 
-        private Node3D _node;
-        private SpringArm3D _springArm;
-        private Camera3D _camera;
+    //    private Node3D _node;
+    //    private SpringArm3D _springArm;
+    //    private Camera3D _camera;
 
         
-    }
+    //}
 }
